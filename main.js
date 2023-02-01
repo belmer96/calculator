c9578f5dca97268156c1b3fc7d73e42822d29f10
belmer96 <elmerblake1500@gmail.com>
2023-02-01 12:12:32 -0600

    finalize the = operator without using eval() function

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ let screenvalue='';
 for (const btn of buttonpress) {
     btn.addEventListener('click', function() {
         const currentb=this.textContent;
-        console.log(this.textContent);
+        const currentid=this.id;
         if (currentb!=='Delete'&&currentb!=='Clear'&&currentb!=='='){
+            if (currentid=='divide'){
+                currentc="/";
+                screenvalue=screenvalue+currentc;
+                document.getElementById('results').textContent=screenvalue;
+            }
+            else{
             screenvalue=screenvalue+currentb;
-            console.log("yo momma "+currentb);
             document.getElementById('results').textContent=screenvalue;
+            }
         }
+          
         if(currentb=='Clear'){
             screenvalue='';
             document.getElementById('results').textContent=0;
@@ -21,12 +28,50 @@ for (const btn of buttonpress) {
             document.getElementById('results').textContent=screenvalue;
         }
         if(currentb=='='){
-            calculate=screenvalue;
-            document.getElementById('results').textContent=("bruh "+calculate);
-
+            try {
+            let calculation = screenvalue;
+            calculation = calculation.replace(/\+/g, ' + ');
+            calculation = calculation.replace(/\-/g, ' - ');
+            calculation = calculation.replace(/\X/g, ' * ');
+            calculation = calculation.replace(/\//g, ' / ');
+    
+            let terms = calculation.split(' ');
+            let result = Number(terms[0]);
+    
+            for (let i = 1; i < terms.length; i += 2) {
+              let operator = terms[i];
+              let operand = Number(terms[i + 1]);
+    
+              switch (operator) {
+                case '+':
+                  result += operand;
+                  break;
+                case '-':
+                  result -= operand;
+                  break;
+                case '*':
+                  result *= operand;
+                  break;
+                case '/':
+                  result /= operand;
+                  break;
+                default:
+                  throw new Error(`Unknown operator: ${operator}`);
+              }
+            }
+    
+            document.getElementById('results').textContent = result;
+          } catch (error) {
+            console.error(error);
+            document.getElementById('results').textContent = 'Error';
+          }
         }
+
+    
+
         
     });
-  }
+}
+
 
 
